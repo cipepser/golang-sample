9c3278e974d4191421e9461a47bd436821621263
cipepser <respepic@gmail.com>
2016-11-05 16:05:18 +0900

    add type.go

---
+++ gihyotest/type.go
@@ -0,0 +1,14 @@
+package main
+
+type ID int
+type Priority int
+
+func ProcessTask(id ID, priority Priority) {
+	
+}
+
+func main() {
+	var id ID = 3
+	var priority Priority = 5
+	ProcessTask(id, priority)
+}
