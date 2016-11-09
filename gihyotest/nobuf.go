21765b8c10b8f709703abdd15209f4df5bfd4f5d
cipepser <respepic@gmail.com>
2016-11-09 22:56:41 +0900

    add nobuf.go

--- gihyotest/nobuf.go
+++ gihyotest/nobuf.go
@@ -1,15 +1,26 @@
 package main
 
 import(
+	"fmt"
 	"time"
 )
 
 func main() {
 	ch := make(chan string)
 	
+	// データが書き込まれるまで待つ場合
+	// go func() {
+	// 	time.Sleep(time.Second)
+	// 	ch <- "a"
+	// } ()
+	// fmt.Println(<-ch) // 書き込まれるのを待つ
+	
+	// 読み出されるまで待つ場合
 	go func() {
+		// fmt.Println(<-ch) 
 		time.Sleep(time.Second)
-		ch <- "a"
+		fmt.Println(<-ch) 
 	} ()
-	<-ch
+	// time.Sleep(time.Second)
+	ch <- "a"
 }
