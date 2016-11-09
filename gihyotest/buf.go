67e0c56267024173d2e6948d2373f0d6eb3f7a51
cipepser <respepic@gmail.com>
2016-11-09 23:08:59 +0900

    add comment for goroutine, block

--- gihyotest/buf.go
+++ gihyotest/buf.go
@@ -1,18 +1,22 @@
 package main
 
 import(
+	"fmt"
 	"time"
 )
 
 func main() {
-	ch := make(chan string, 3)
+	ch := make(chan string, 2)
 	
 	go func() {
-		time.Sleep(time.Second)
-		<-ch
+		time.Sleep(time.Second * 3) // 3秒待つ
+		fmt.Println("1:" + <-ch)
 	} ()
-	ch <- "a"
-	ch <- "b"
-	ch <- "c"
-	ch <- "d"
+	ch <- "a" // 1つ目のバッファに書き込む
+	ch <- "b" // 2つ目のバッファに書き込む
+	fmt.Println("2:" + <-ch) // 3秒経つ前にaが読み出される
+	ch <- "c" // 3秒経つ前にcが書き込まれる
+	ch <- "d" // 3秒経つ前だと2つのバッファを使い切っているのでブロック
+	fmt.Println("3:" + <-ch)
+	fmt.Println("4:" + <-ch)
 }
